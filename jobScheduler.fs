a6a891e827c7eaaa23bf9303e23ad0e3f497352f
millacat <camillakd@msn.com>
2021-11-10 12:41:18 +0100

    Schedule jobs using refs

--- jobScheduler.fs
+++ jobScheduler.fs
@@ -16,17 +16,43 @@ let dummyJob = {
     Submitted   = DateTime.Now
 }
 
-let SIZE_QUEUE = 4
-let QUEUE = Array.create SIZE_QUEUE dummyJob
-let mutable FST_INDEX  = 0
-let mutable LST_INDEX  = 0
+type Queue = {
+    mutable Size : int
+    mutable Queue : Job []
+    mutable Fst_index : int
+    mutable Lst_index : int
+}
 
+let init_queue (size : int) : Job [] = Array.create size dummyJob
+//let mutable QUEUE : Job [] * Job [] = (init_queue SIZE_PRIORITY_Q, init_queue SIZE_Q)
+let mutable SIZE_QUEUE  = 4
+let mutable QUEUE       = init_queue SIZE_QUEUE
+let mutable FST_INDEX_Q = 0
+let mutable LST_INDEX_Q = 0
+
+let mutable SIZE_PRIORITY  = 4
+let mutable QUEUE_PRIORITY = init_queue SIZE_PRIORITY
+let mutable FST_INDEX_PRIORITY  = 0
+let mutable LST_INDEX_PRIORITY  = 0
+
+let increaseSizeQueue (queue : outref<Job []>) (size : byref<int>) : unit =
+    let extraSpace = init_queue size in
+    queue <- Array.append queue extraSpace
+    size  <- size * 2
+
+let insertJob (queue : outref<Job []>) (job : Job) (lstIndex : byref<int>)
+              (size : byref<int>) : unit =
+    queue.[lstIndex] <- job
+    if lstIndex >= size - 1 then
+        increaseSizeQueue &queue &size
+    else ()
+    lstIndex <- lstIndex + 1
 
 let scheduleJob (job : Job) : unit =
-    QUEUE.[LST_INDEX] <- job
-    (* Check if we need to allocate a bigger queue *)
-
-    LST_INDEX <- LST_INDEX + 1
+    if job.HasPriority then
+        insertJob &QUEUE_PRIORITY job &LST_INDEX_PRIORITY &SIZE_PRIORITY
+    else
+        insertJob &QUEUE job &LST_INDEX_Q &SIZE_QUEUE
 
 let runNextJob unit : unit =
 // 1. If there is any job older than 5 seconds, it should run.
--- testing.fsx
+++ testing.fsx
@@ -4,14 +4,20 @@ open System
 printfn "##### Testing of the Job Scheduler #####"
 
 let dummyJob = { Id = 0; Duration = 0; HasPriority = false; Submitted = DateTime.Now }
-let j1 = {Id = 1; Duration = 3; HasPriority = true; Submitted = DateTime.Now}
+let j1 = {Id = 1; Duration = 3; HasPriority = false; Submitted = DateTime.Now}
 let j2 = {Id = 2; Duration = 3; HasPriority = true; Submitted = DateTime.Now}
 printfn "%A" <| (j1.Submitted > j2.Submitted)
 
 printfn "###  Testing scheduleJob (job : Job) : unit ###"
 
 scheduleJob j1
-scheduleJob j2
-scheduleJob j2
-scheduleJob j2
-printfn "%A" QUEUE
+for int i = 0 to 5 do
+    scheduleJob j2
+scheduleJob j1
+printfn "QUEUE %A\n" QUEUE
+printfn "PRIORITY %A" QUEUE_PRIORITY
+
+let vars = [SIZE_QUEUE; FST_INDEX_Q; LST_INDEX_Q; SIZE_PRIORITY; FST_INDEX_PRIORITY; LST_INDEX_PRIORITY]
+for var in vars do
+    printfn "%A" var
+
